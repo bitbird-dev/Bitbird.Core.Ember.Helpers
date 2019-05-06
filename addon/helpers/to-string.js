6f7f4a8f1a722f61eca80037ea78bf7fd71bb175
Martin Liboswar <m.liboswar.bitbird@outlook.com>
2019-05-06 11:03:33 +0200

    1.0.10 Adds pretty print to to-string for json

--- addon/helpers/to-string.js
+++ addon/helpers/to-string.js
@@ -3,9 +3,13 @@ import { helper } from '@ember/component/helper';
 export function toString(params/*, hash*/) {
   if (params.length === 0 || params[0] === null || params[0] === undefined) return null;
 
-  let parseJson = !!params[1];
+  let parseJson = !!params[1],
+    prettyPrint = params[1] === "pretty";
 
   if(typeof params[0] === 'object' && parseJson) {
+    if(prettyPrint) {
+      return JSON.stringify(params[0], null, ' ');
+    }
     return JSON.stringify(params[0]);
   }
 
